8392622b6b0e8dab5d3285955b81516b69b1aa9e
garrettkatz <garrett.katz@gmail.com>
2017-04-15 12:39:23 -0400

    t+b |alpha| progress

--- tb_combo.py
+++ tb_combo.py
@@ -24,7 +24,20 @@ def union(fxV1, fxV2):
     fxV = rfx.get_unique_points_recursively(fxV, neighbors=neighbors)
     return fxV 
 
-N = 3
+def add_alpha_mins(W, VA, fxV):
+    N = W.shape[0]
+    abs_alpha = np.fabs(VA[N,:])
+    local_mins = (abs_alpha[:-2] < abs_alpha[1:-1]) & (abs_alpha[1:-1] < abs_alpha[2:])
+    seed_mask = np.zeros(abs_alpha.shape,dtype=bool)
+    seed_mask[:-2] |= local_mins
+    seed_mask[1:-1] |= local_mins
+    seed_mask[2:] |= local_mins
+    slowV = VA[:N, seed_mask]
+    fxV = np.concatenate((fxV,slowV),axis=1)
+    fxV, _ = rfx.post_process_fxpts(W, fxV)
+    return fxV
+
+N = 10
 all_cloops = 0.
 all_comps = 0.
 for samp in range(50):
@@ -33,6 +46,9 @@ for samp in range(50):
          'B': fe.load_npz_file('results/baseline_full_base_N_%d_s_%d.npz'%(N,samp))}
     W = npz['T']['W']
     c = npz['T']['c']
+
+    # add alpha mins
+    npz['T']['fxV_unique'] = add_alpha_mins(W, npz['T']['VA'], npz['T']['fxV_unique'])
     
     k = 0 # current component
     
@@ -58,21 +74,29 @@ for samp in range(50):
         status, fxV, VA, c, _, _, _ = rfx.traverse(W, va=va, c=c, max_traverse_steps = 2**20)
         statuses.append(status)
     
-        # if status=='Success':
+        if status=='Success':
+        
+            #N!=3:
             # pt.plotNd(npz['T']['VA'],3*np.ones((N,1))*np.array([[-1,1]]),'r-')
             # pt.plotNd(VA,3*np.ones((N,1))*np.array([[-1,1]]),'b-')
+
+            # #N=3:
             # ax = pt.plt.gca(projection='3d')
             # pt.quiver(ax, npz['T']['VA'][:N,:], c*npz['T']['VA'][[N],:])
             # pt.plot(ax, npz['T']['VA'][:N,:],'r-')
             # pt.plot(ax, npz['T']['fxV_unique'][:N,:],'ko')
             # pt.plot(ax, seed[k],'go')
             # pt.plot(ax, VA[:N,:],'b-')
-            # pt.plot(ax, -VA[:N,:],'b-')
+            # # pt.plot(ax, -VA[:N,:],'b-')
+
+            # # alpha:
             # pt.plt.plot(npz['T']['VA'][N,:])
             # pt.plt.plot(np.zeros(npz['T']['VA'][N,:].shape))
             # pt.plt.ylim([-1,1])
-            # print('%f'%np.fabs(np.tanh(W.dot(seed[k]))-seed[k]).max())
+            # print('%g'%np.fabs(np.tanh(W.dot(seed[k]))-seed[k]).max())
+            # print('%g'%np.fabs(found[0]-seed[k]).max(axis=0).min())
             # raw_input('...')
+            pass
     
         # need rfx.post_process to use simpler neighbor!
         fxV = np.concatenate((seed[k],fxV),axis=1) # be sure to include seed(t)
@@ -92,5 +116,6 @@ for samp in range(50):
     all_cloops += cloops
     all_comps += len(statuses)-1
     # Not all cloops all the time! Local minima of |alpha| that don't change sign!!
+        
 
 print('In all, %d cloops of %d comps'%(all_cloops,all_comps))
