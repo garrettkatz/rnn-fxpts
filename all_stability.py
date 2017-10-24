8d92171731fd175f431e92a35f0bfbfcc5cb1984
garrettkatz <garrett.katz@gmail.com>
2017-10-24 12:32:04 -0400

    tested stability script

--- all_stability.py
+++ all_stability.py
@@ -4,10 +4,10 @@ num_procs = 0
 
 test_data_ids = [
     "full_base",
-    "big256",
-    "big512",
-    "big1024",
+    "big256_base",
+    "big512_base",
+    "big1024_base",
 ]
 
-for test_data_id in test_data_ids
+for test_data_id in test_data_ids:
     _ = fe.run_TvB_stability_experiments(test_data_id,num_procs)
